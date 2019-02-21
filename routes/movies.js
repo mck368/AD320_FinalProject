b8403b0973331e1797a1ca2e9ed512df683bae5c
rbeeler <ruthbeeler3@gmail.com>
2019-02-20 17:30:09 -0800

    with database
    needs some work - this will show the contents of the database but loses the header etc..

---
+++ routes/movies.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+//connect to database
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'iLoveMovies'
+});
+/*display contents of movie table from database*/
+router.get('/', function(req, res, next) {
+  connection.connect(function(err) {
+    if (err) throw err;
+    connection.query("SELECT * FROM movieInfo", function (err, result, fields) {
+      if (err) throw err;
+      res.send(result);
+      console.log('connected to database');
+    });
+  });
+});
+
+/* GET movies page.*/
+router.get('/', function(req, ress, next) {
+  ress.render('movies', { title: 'Movies' });
+});
+
+
+module.exports = router;
