19df396ab9e3bf5ea56d08fc86dd37b9e8c0347d
Thomas <tcoducayen@gmail.com>
2019-02-21 17:44:17 -0800

    uodated routes to display proper titles and added a movie and user route

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET movies page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'iLoveMovies' });
+  res.render('movies', { title: 'Movies' });
 });
 
 module.exports = router;
