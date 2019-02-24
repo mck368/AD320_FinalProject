d064280da211cf24bcbe5b4e1d4bd405b22ed24a
rbeeler <ruthbeeler3@gmail.com>
2019-02-23 22:13:14 -0800

    changed header friends to users

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql');
-
-//connect to database
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'iLoveMovies'
-});
-
-/*display contents of users table from database*/
-router.get('/', function(req, res,) {
-  connection.connect(function(err) {
-    if (err) throw err;
-    connection.query("SELECT * FROM users", function (err, result, fields) {
-      if (err) throw err;
-      res.send(result);
-    });
-  });
-});
-
-/* GET users page - listing. */
+/* GET users page */
 router.get('/', function(req, res, next) {
   res.render('users', { title: 'Users' })
   res.send('respond with a resource');
