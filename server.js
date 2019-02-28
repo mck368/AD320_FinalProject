2233304d44256cd0b73e9de12ff4496ae4296cc3
adslin <shihpei.ling@gmail.com>
2019-02-27 23:39:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-    return res.send({ error: true, message: 'Hello World!!' })
+    return res.send({ error: true, message: 'Hello, I Love Movies!!' })
 });
 
 app.get('/movies', function (req, res) {
@@ -28,10 +28,47 @@ app.get('/movies/:id', function (req, res) {
 });
 
 app.get('/movies/search/:keyword', function (req, res) {
+    
     let keyword = req.params.keyword;
-    mc.query("SELECT * FROM movieinfo WHERE Title LIKE ? ", ['%' + keyword + '%'], function (error, results, fields) {
+    
+    mc.query('SELECT * FROM movieinfo WHERE Title LIKE ? ', ['%' + keyword + '%'], function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'Movie search list.' });
+    });
+});
+
+app.post('/movies/add/:title', function (req, res) { 
+    
+    let title = req.params.title; 
+    
+    if (!title) {
+        return res.status(400).send({ error:true, message: 'Please provide title' });
+    } 
+
+    mc.query('INSERT INTO movieinfo SET ? ', { title: title }, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'New movie has been created successfully.' });
+    });
+});
+
+app.put('/movies/update/:id', function (req, res) { 
+
+    let movie_id = req.body.id;
+    let title = req.body.title;
+ 
+    mc.query('UPDATE movieinfo SET Title = ? WHERE MovieID = ?', [ movie_id, title ], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Movie has been updated successfully.' });
+    });
+}); 
+
+app.delete('/movies/del/:id', function (req, res) {
+ 
+    let movie_id = req.params.id;
+ 
+    mc.query('DELETE FROM movieinfo WHERE MovieID = ?', [movie_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Movie has been updated successfully.' });
     });
 });
 
