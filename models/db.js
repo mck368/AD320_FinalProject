dc77bd8cb723c0864bc10a1c2754691c9bbca38a
adslin <shihpei.ling@gmail.com>
2019-03-06 11:24:29 -0800

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,8 @@
 'user strict';
 
 var mysql = require('mysql');
-var dbCred = require('./db.json');
+var dbCred = require('../db.json');
+var mc = mysql.createConnection(dbCred);
 
 mc.connect(function(err) {
     if (err) throw err;
