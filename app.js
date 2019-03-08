1ef490f3140a0404a3ec50cf9626c72e48f5b970
adslin <shihpei.ling@gmail.com>
2019-03-08 14:18:50 -0800

    update app.js

--- app.js
+++ app.js
@@ -50,19 +50,16 @@ app.use('/blog_seattlemovies', blog_SeattleRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
-// localhost:3000
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000...')
-})
 
 module.exports = app;
