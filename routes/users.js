fb612f10f5535e620bd3b1ce841fca7944af80c4
Maria Kalusz <mkalusz1@gmail.com>
2019-03-12 17:45:29 -0700

    connecting to ../db instead

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 var mysql = require('mysql');
-
-//connect to database
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'password',
-  database : 'iLoveMovies'
-});
 
 /*display contents of users table from database*/
 router.get('/', function(req, res,) {
