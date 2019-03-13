8c79ef404d09e1c642016294591c7c0794331d65
Maria Kalusz <mkalusz1@gmail.com>
2019-03-12 20:52:40 -0700

    more hcanges

--- routes/friends.js
+++ routes/friends.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db');
 
 /* GET friends page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,16 @@ router.get('/', function(req, res, next) {
   }
 
   res.render('friends', data);
+});
+
+/*display all contents of movie table from database*/
+router.get('/api/all', function(req, res, next) {
+  db.query("SELECT * FROM users", function (err, result, fields) {
+    if (err) {
+      res.status(500).send(error);
+    }
+    res.send(result);
+  });
 });
 
 module.exports = router;
