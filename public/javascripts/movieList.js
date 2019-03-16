24b7a810849946efded5a835e848f0e94263b071
adslin <shihpei.ling@gmail.com>
2019-03-16 09:06:50 -0700

    add movie info

---
+++ public/javascripts/movieList.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+	$("img[src='images/ace_ventura.jpg']").click(function() {
+        $("<p><b>A goofy detective specializing in animals goes in search of the missing mascot of the Miami Dolphins.</b></p>").appendTo(".message#0");
+
+   });
+
+    $("img[src='images/big_daddy.jpg']").click(function() {
+        $("<p><b>A lazy law school grad adopts a kid to impress his girlfriend, but everything doesn&apos;t go as planned and he becomes the unlikely foster father.</b></p>").appendTo(".message#1");    
+	});
+
+	$("img[src='images/elf.jpg']").click(function() {
+        $("<p><b>After discovering he is a human, a man raised as an elf at the North Pole decides to travel to New York City to locate his real father.</b></p>").appendTo(".message#2");
+
+   });
+
+	$("img[src='images/glass.jpg']").click(function() {
+        $("<p><b>Security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities.</b></p>").appendTo(".message#3");
+
+   });
+
+	$("img[src='images/happy_gilmore.jpg']").click(function() {
+        $("<p><b>A rejected hockey player puts his skills to the golf course to save his grandmother's house.</b></p>").appendTo(".message#4");
+
+   });
+
+	$("img[src='images/inception.jpg']").click(function() {
+        $("<p><b>A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.</b></p>").appendTo(".message#5");	
+
+   });
+
+	$("img[src='images/lego_movie.jpg']").click(function() {
+        $("<p><b>An ordinary LEGO construction worker, thought to be the prophesied as special, is recruited to join a quest to stop an evil tyrant from gluing the LEGO universe into eternal stasis.</b></p>").appendTo(".message#6");
+
+   });
+
+	$("img[src='images/matrix.jpg']").click(function() {
+        $("<p><b>A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.</b></p>").appendTo(".message#7");
+
+   });
+
+	$("img[src='images/murder_by_numbers.jpg']").click(function() {
+        $("<p><b>Two gifted high school students execute a perfect murder - then become engaged in an intellectual contest with a seasoned homicide detective.</b></p>").appendTo(".message#8");
+
+   });
+
+	$("img[src='images/requiem_for_a_dream.jpg']").click(function() {
+        $("<p><b>The drug-induced utopias of four Coney Island people are shattered when their addictions run deep.</b></p>").appendTo(".message#9");
+
+   });
+
+	$("img[src='images/truman.jpg']").click(function() {
+        $("<p><b>An insurance salesman discovers his whole life is actually a reality TV show.</b></p>").appendTo(".message#10");
+
+   });
+
+ 	$("img[src='images/50_first_dates.jpg']").click(function() {
+        $("<p><b>Henry Roth is a man afraid of commitment up until he meets the beautiful Lucy. They hit it off and Henry think he&apos;s finally found the girl of his dreams, until he discovers she has short-term memory loss and forgets him the next day.</b></p>").appendTo(".message#11");
+
+   });
+});
